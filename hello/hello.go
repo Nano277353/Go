1079255ea94acf44ec58ca6782888fc4811f79ab
Nano <emilianormz2706@gmail.com>
2026-05-28 18:55:00 -0600

    Trying out  Go

---
+++ hello/hello.go
@@ -0,0 +1,5 @@
+package main //The main package is the entry point of the program and identifies an executable program.
+import "fmt" //We use the import keyword to import a package. fmt is a built-in package provided by Go that provides input/output utility functions.
+func main() { //Main function is where program starts.
+	fmt.Println("Hello world")
+}
---
+++ variables/variables.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+//var age = 20
+
+func main() {
+	//var age = 20
+	fmt.Println("Hello, World!")
+}
