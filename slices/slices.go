e6856383a43058d983f7a60930092b9d26d7779e
Nano <emilianormz2706@gmail.com>
2026-05-29 23:37:47 -0600

    More changes

--- slices/slices.go
+++ slices/slices.go
@@ -17,6 +17,17 @@ func main() {
 	S4 := S3[2:5] // S4 has length 3 and capacity 3. Takes elements from index 2 to 4 (8 9 10)
 	printSlice("S4", S4)
 	println("Takes elements from index 2 to 4 from S3 (8 9 10)")
+
+	myArray := [3]string{"First", "Second", "Third"}
+	fmt.Println("myArray:", myArray)
+	mySlice := myArray[:]
+	mySlice2 := myArray[:]
+	mySlice[0] = "test"
+	fmt.Println("myArray:", myArray)
+	fmt.Println("First element of mySlice2:", mySlice2[0])
+	fmt.Println("First element of myArray:", myArray[0])
+	fmt.Println("mySlice and mySlice2 are views of the same array, updating any of them updates the other and the underlying array.")
+
 }
 
 func printSlice(s string, x []int) {
